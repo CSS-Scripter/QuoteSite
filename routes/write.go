e494d30da27996d7aa352dd5835247e10e0c6413
Thom Leenman <thomleenman@gmail.com>
2020-07-13 00:37:34 +0200

    Added validation for the quite

--- routes/write.go
+++ routes/write.go
@@ -11,20 +11,37 @@ import (
 func Write(c *fiber.Ctx) {
 	c.Accepts("json", "text")
 	c.Accepts("application/json")
-	writeToFile(c.Body())
-	c.Send(c.Body())
+	succes := writeToFile(c.Body())
+	var message string
+	if succes {
+		message = `{"status":"200","message":"Quote created"}`
+	} else {
+		message = `{"status":"400","message":"Bad request"}`
+	}
+	c.Send(message)
 }
 
-func writeToFile(content string) {
+func isQuoteValid(quote Quote) bool {
+	if quote.Message == "" || quote.By == "" || quote.Year == "" {
+		return false
+	}
+	return true
+}
+
+func writeToFile(content string) bool {
 	quotes := readQuotes()
 	var quote Quote
 	err := json.Unmarshal([]byte(content), &quote)
 	check(err)
-	quotes = append([]Quote{quote}, quotes...)
 
+	if !isQuoteValid(quote) {
+		return false
+	}
+
+	quotes = append([]Quote{quote}, quotes...)
 	quoteString, err := json.Marshal(quotes)
 	check(err)
-
 	err = ioutil.WriteFile("quotes.json", []byte(quoteString), 0644)
 	check(err)
+	return true
 }
